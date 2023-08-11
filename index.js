0c37fc18f583d2bded89177dbb6fdebe0f2c35b5
Cosq <wpinilla@unal.edu.co>
2023-08-10 20:41:52 -0500

    Renombrando variables

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const apiKey = "07fd0794b848eb5d10d388c9aa4d360a";
-const weatherSection = document.querySelector('.main-weathers');
+const cardsSection = document.querySelector('.main-Cards');
 const input = document.querySelector('input');
 const buscar = document.querySelector('.buscador');
 const portafolio = document.querySelector('.website');
@@ -11,43 +10,44 @@ portafolio.addEventListener('click',()=>{
 const closeCard = (childNode)=>{
   childNode.parentNode.removeChild(childNode);
 }
-const createWeatherCard = (city, desc, temp, icon)=>{
+const createPokeCard = (nombre, desc, altura, icon)=>{
   const divContenedor = document.createElement('div')
   divContenedor.classList.add("divContenedor")
-  const divCiudad = document.createElement('h2')
-  divCiudad.innerText = city;
-  divCiudad.classList.add('tituloCiudad');
+  const divNombre = document.createElement('h2')
+  divNombre.innerText = nombre;
+  divNombre.classList.add('tituloPoke');
   const divDescripcion = document.createElement('h3')
   divDescripcion.innerText = desc;
-  divDescripcion.classList.add('descripcionClima');
-  const iconWeather = document.createElement('img');
-  iconWeather.src = `${icon}`;
-  iconWeather.classList.add('imagenClima');
-  const divTemperatura = document.createElement('h2')
-  divTemperatura.innerText = `${temp} m`;
+  divDescripcion.classList.add('descripcionPoke');
+  const iconPoke = document.createElement('img');
+  iconPoke.src = `${icon}`;
+  iconPoke.classList.add('imagenPoke');
+  const divAltura = document.createElement('h2')
+  divAltura.innerText = `${altura} m`;
   const botonBorrar = document.createElement('span');
   botonBorrar.classList.add('botonBorrar');
-  divContenedor.append(divCiudad, divDescripcion,iconWeather, divTemperatura, botonBorrar);
-  weatherSection.append(divContenedor);
+  divContenedor.append(divNombre, divDescripcion,iconPoke, divAltura, botonBorrar);
+  cardsSection.append(divContenedor);
   botonBorrar.addEventListener('click',()=>{
     divContenedor.remove();
   })
 }
 
-const funcionAgregar = async (ciudad = 'bogota')=>{
-  const fet = await window.fetch(`https://pokeapi.co/api/v2/pokemon/ditto`);
-  const salida = await fet.json();
+const funcionAgregar = async (Pokemon)=>{
+  debugger;
   try{
-    createWeatherCard(salida?.name, `id:${salida?.id}`, `Altura:${salida.height}`,salida.sprites.front_default);
+    const fet = await window.fetch(`https://pokeapi.co/api/v2/pokemon/${Pokemon}`);
+    const salida = await fet.json();
+    createPokeCard(salida?.name, `id:${salida?.id}`, `Altura:${salida.height}`,salida.sprites.front_default);
   }
   catch{
-    window.alert('Lo sentimos, no pudimos encontrar esta ciudad :(');
+    window.alert('Lo sentimos, no pudimos encontrar este Pokemon :(');
   }
 }
-const retornoCiudad = ()=>{
+const retornoPokemon = ()=>{
   funcionAgregar(input.value);
   input.value = '';
 }
-buscar.addEventListener('click', retornoCiudad);
+buscar.addEventListener('click', retornoPokemon);
 
 
